ebaeb5ad9ba6a1945607b50a2c6bafc070462bf2
Yauheni Karaniuk <ykaraniuk@fixnetwork.com>
2025-03-14 16:04:42 -0400

    - Updated game status to "say goodbyes" to lost languages. - Added a hidden accessibility section that updates screen reader users with live updates about game status, current guesses and remaining guesses.

--- src/components/AssemblyGame.jsx
+++ src/components/AssemblyGame.jsx
@@ -1,24 +1,13 @@
 import { languages } from "../libs/languages";
+import { getFarewellText } from "../libs/utils";
 import { useState } from 'react';
 import { clsx } from "clsx"
 
 export default function AssemblyEndgame() {
 
     //State Variables
-    const [currentWord, setCurrentWord] = useState("react");
+    const [currentWord, setCurrentWord] = useState("refactor");
     const [guessedLetters, setCurrentGuesses] = useState([]);
-    
-    //Derived values
-    const wrongGuessCount = guessedLetters
-        .filter(letter => !currentWord
-        .includes(letter))
-        .length;
-    
-    const isGameLost = wrongGuessCount >= languages.length - 1;
-    const isGameWon = currentWord
-        .split("")
-        .every(letter => guessedLetters.includes(letter));
-    const isGameOver = isGameWon || isGameLost;
 
     function addGuessedLetter(letter) {
         setCurrentGuesses(prevGuesses => 
@@ -28,23 +17,54 @@ export default function AssemblyEndgame() {
         );
     }
 
-    //Static Values
-    const gameWinStatus = 
-        <>
-            <h2>You Win!</h2>
-            <p>Well done! 🎉</p>
-        </>;
+    //Derived values
+    const wrongGuessCount = guessedLetters
+        .filter(letter => !currentWord
+        .includes(letter))
+        .length;
     
-    const gameLostStatus = 
-    <>
-        <h2>Game Over!</h2>
-        <p>You lose! Better start learning Assembly 😭</p>
-    </>;
+    const lostLanguage = wrongGuessCount > 0 ? languages.at(wrongGuessCount-1).name : "";
+    
+    const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
+    const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter);
+    const numberOfGuessesLeft = (languages.length - 1) - wrongGuessCount;
+    
+    const isGameLost = wrongGuessCount >= languages.length - 1;
+    const isGameWon = currentWord
+        .split("")
+        .every(letter => guessedLetters.includes(letter));
+    const isGameOver = isGameWon || isGameLost;
 
+    //Static Values
+    function renderGameStatus() {
+        if(wrongGuessCount > 0 && !isGameOver) {
+            return(
+                <span>{getFarewellText(lostLanguage)}</span>
+            );
+        } else if(isGameWon) {
+            return(
+                <>
+                    <h2>You Win!</h2>
+                    <p>Well done! 🎉</p>
+                </>
+            );
+        } else if(isGameLost) {
+            return(
+                <>
+                    <h2>Game Over!</h2>
+                    <p>You lose! Better start learning Assembly 😭</p>
+                </>
+            );    
+        } else {
+            return null;
+        }
+    }
+   
     const gameStatusClass = clsx("game-status", {
         'in-progress': !isGameOver,
         won: isGameWon,
         lost: isGameLost,
+        'lost-language': lostLanguage.length > 0 && !isGameOver,
     })
 
     const currentWordElements = currentWord.split("").map((letter, index) => {
@@ -59,9 +79,12 @@ export default function AssemblyEndgame() {
     });
     
     const languageElements = languages.map((lang, index) => {
+        
+        const isLostLanguage = index < wrongGuessCount;
         const className = clsx("language-chip", {
-            lost: index < wrongGuessCount
+            lost: isLostLanguage,
         });
+        
         return(
             <span
             key={lang.name}
@@ -79,7 +102,6 @@ export default function AssemblyEndgame() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     
     const keyboardElement = alphabet.split("").map((letter, index) => {
-        
         const isGuessed = guessedLetters.includes(letter)
         const isCorrect = isGuessed && currentWord.includes(letter)
         const isWrong = isGuessed && !currentWord.includes(letter)
@@ -93,12 +115,14 @@ export default function AssemblyEndgame() {
             <button
                 key={index + letter}
                 className={className}
+                disabled={isGameOver}
+                aria-disabled={guessedLetters.includes(letter)}
+                aria-label={`letter ${letter}`}
                 onClick={() => addGuessedLetter(letter)}
             >
                 {letter.toUpperCase()}
             </button>
         );
-       
     })
 
     return(
@@ -110,20 +134,45 @@ export default function AssemblyEndgame() {
                 programming world safe from Assembly!</p>
             </header>
             
-            <section className={gameStatusClass}>
-                {isGameLost && gameLostStatus}
-                {isGameWon && gameWinStatus}
+            <section 
+                aria-live="polite" 
+                role="status" 
+                className={gameStatusClass}
+            >
+                {renderGameStatus()}
             </section>
+            
             <section className="language-box">
                 {languageElements}
             </section>
+            
             <section className="guess-container">
                 {currentWordElements}    
             </section>
+            
+            <section 
+                className="sr-only" 
+                aria-live="polite" 
+                role="status"
+            >
+                <p>
+                    {currentWord.includes(lastGuessedLetter) ?
+                        `Correct! The letter ${lastGuessedLetter} is in the word.` :
+                        `Sorry, the letter ${lastGuessedLetter} is not in the word.`
+                    }
+                    {`You have ${numberOfGuessesLeft} attempts left.`}
+                </p>
+                <p>Current word: {currentWord.split("").map(letter =>
+                    guessedLetters.includes(letter) ? letter + "." : "blank.").join(" ")}
+                </p>
+            </section>
+
             <section className="keyboard">
               {keyboardElement}
             </section>
+            
             {isGameOver ? <button className="new-game">New Game</button> : ""}
+
         </main>
         </>
     );
